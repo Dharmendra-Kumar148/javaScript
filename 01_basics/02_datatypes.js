7ca54d69940e36a6627fc318d224483d7f11ddf8
avail <87807596+Dharmendra-Kumar148@users.noreply.github.com>
2023-09-29 12:02:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+// alert() //we are using node js  not browser
+
+console.log(3+3)
+console.log("abhay")
+
+let name = "yadav"
+let age = 28
+let isLoggedIn =false
+
+/*
+number => 2^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique 
+*/
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
