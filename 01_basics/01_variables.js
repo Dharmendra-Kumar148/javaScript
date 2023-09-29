a7121b156bd6bb0e439913656a274f615973306a
avail <87807596+Dharmendra-Kumar148@users.noreply.github.com>
2023-09-29 10:58:32 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144533
+let accountEmail = "abhay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kanpur"
+let accountState;
+
+/*
+let not used var
+*/
+
+// accountId = 2 //not allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
