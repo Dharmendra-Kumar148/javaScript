9ad5148b553f02b8be377f6be288b502bc763ca9
avail <87807596+Dharmendra-Kumar148@users.noreply.github.com>
2023-10-01 12:22:33 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(score);
+// console.log(typeof score);
+// console.log(score);
 
 let valueInNo = Number(score)
 
-console.log(typeof valueInNo);
-console.log(valueInNo);
+// console.log(typeof valueInNo);
+// console.log(valueInNo);
 
 /*
 "33" => 33
@@ -19,7 +19,7 @@ false => 0
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1 => true
@@ -31,5 +31,38 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNo = String(someNumber)
-console.log(stringNo);
-console.log(typeof stringNo);
+// console.log(stringNo);
+// console.log(typeof stringNo);
+
+
+
+// ***************************** operations ******************************************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "abhay"
+let str2 = " yadav"
+
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
